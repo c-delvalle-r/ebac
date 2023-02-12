03617b1ca6b1002bb8b274b416d6e9d35bdf6303
Carlos Del Valle <delvallereyes.carlos@gmail.com>
2023-02-12 13:12:28 -0600

    Se agrega un print "Este cambio se hace para la actividad del módulo 22

--- Python/Modulo21y22/commit_test.py
+++ Python/Modulo21y22/commit_test.py
@@ -10,3 +10,5 @@ current_time = time.strftime('%H:%M:%S', t)
 print(current_time)
 
 # Test comment for Source Tree commits and pushes
+
+print('Este cambio se hace para la actividad del Módulo 22')
